b865f7d02e1177b63bf98f72e06bdae9abcc3fd0
asaldana-fs <asaldana@financialsolutions.mx>
2021-06-10 14:18:44 -0500

    Initial version of Microservice generated by JHipster-7.0.1

---
+++ src/main/java/jhipster/example/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package jhipster.example.web.rest;
---
+++ src/test/java/jhipster/example/IntegrationTest.java
@@ -0,0 +1,17 @@
+package jhipster.example;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import jhipster.example.MicroserviceApp;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = MicroserviceApp.class)
+public @interface IntegrationTest {
+}
